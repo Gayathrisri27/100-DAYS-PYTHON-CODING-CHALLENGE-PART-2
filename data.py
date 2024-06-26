86ad39b15f86b82eaa2cf3c41eff1b76340ff89c
Gayathrisri27 <110322325+Gayathrisri27@users.noreply.github.com>
2024-06-26 22:58:19 +0530

    Delete data.py

--- data.py
+++
@@ -1,7 +0,0 @@
-# data.py
-question_data = [
-    {"question": "Is the sky blue?", "correct_answer": "True"},
-    {"question": "Do cows fly?", "correct_answer": "False"},
-    {"question": "Is Python a programming language?", "correct_answer": "True"},
-    # Add more questions as needed
-]
